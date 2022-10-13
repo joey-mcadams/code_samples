14192dee503d35e8efcf1dffe2212244f001d81d
Joey McAdams <joey.mcadams@cyware.com>
2022-10-13 16:58:49 -0500

    done

--- make_palendromes.py
+++ make_palendromes.py
@@ -1,13 +1,13 @@
-def is_palindrome(in_set: str):
-    in_set_reversed = in_set[::-1]
-    if in_set == in_set_reversed:
+def is_palindrome(in_string: str):
+    in_set_reversed = in_string[::-1]
+    if in_string == in_set_reversed:
         return True
     return False
 
 
 def find_longest_palindrome(in_string: str):
     max_palindrome = ""
-    for i in range(0, len(in_string)):
+    for i in range(0, len(in_string) + 1):
         for j in range(0, i):
             substring = in_string[j:i]
             if is_palindrome(substring):
@@ -16,6 +16,6 @@ def find_longest_palindrome(in_string: str):
     return max_palindrome
 
 
-test_string = "aababacab"
+test_string = "asdfaaaaaaaaaasdf"
 biggest = find_longest_palindrome(test_string)
 print(f"The largest palindrome was {biggest}")
