e0da16286d51fc01fc309d2406d722e4ea20aef4
Joey McAdams <chakan2@hotmail.com>
2023-03-08 08:46:30 -0600

    refactor, added liars dice

---
+++ liars_dice/liars_dice.py
@@ -0,0 +1,18 @@
+a = [1, 1, 1, 3, 4]
+b = [3, 3, 3, 1, 2]
+
+
+def get_duplicates(arr):
+    max_dups = 0
+    target_num = 0
+    for num in arr:
+        count = arr.count(num)
+        if count > max_dups:
+            max_dups = count
+            target_num = num
+
+    return max_dups, target_num
+
+
+print(get_duplicates(b))
+print(get_duplicates(a))
