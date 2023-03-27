73571aa48282b22c036534d6e239ac2f1866f225
Joey McAdams <chakan2@hotmail.com>
2023-03-27 13:35:43 -0500

    fixed tax calculator

--- tax_calculator/tax_calculator.py
+++ tax_calculator/tax_calculator.py
@@ -1,40 +1,72 @@
-def calculate(amount, percent):
-    return (amount * percent) / 100
+"""
+Given a list of progressive tax brackets, write a solution to calculate the tax on a given income.
+
+Let's start with the following tax brackets:
+You are taxed 10% on the first $10,000, 20% on the next $20,000, 30% on the next $15,000 and 50% on any amount remaining
+
+Example:
+  Input: 100,000
+  Output: 37,000
+
+  As you know, taxes can change from year to year. Update your solution to store the different annual tax brackets and incomes to allow for historical comparisons.
+
+  You are taxed 10% on the first $5,000, 20% on the next $30,000, and 50% on any amount remaining
+
+tax_brackets_list = {
+  "2001": [
+    (10_000, .1),
+    (30_000, .2),
+    (45_000, .3)
+  ],
+  "2002": [
+    (5_000, .1),
+    (30_000, .2),
+  ]
+}
+"""
+
+tax_brackets = {
+  "2001": [
+    (10_000, .1),
+    (20_000, .2),
+    (15_000, .3),
+    (0, .5)
+  ],
+  "2002": [
+    (5_000, .1),
+    (30_000, .2),
+    (0, .5)
+  ]
+}
 
 
-def calculate_income_tax(total_income: float) -> float:
-    if total_income <= 250000:
+def calculate_taxes_from_bracket(income: float, tax_brackets: list):
+    tax_responsiblity = 0
+    rolling_income_total = 0
+
+    if income == 0:
         return 0
-    elif total_income <= 500000:
-        return calculate(total_income -
-                         250000, 5)
-    elif total_income <= 750000:
-        return calculate(total_income -
-                         500000, 10) + 12500
-    elif total_income <= 1000000:
-        return calculate(total_income -
-                         750000, 15) + 37500
-    elif total_income <= 1250000:
-        return calculate(total_income -
-                         1000000, 20) + 75000
-    elif total_income <= 1500000:
-        return calculate(total_income -
-                         1250000, 25) + 125000
-    else:
-        return calculate(total_income -
-                         1500000, 30) + 187500
+
+    for i, tax_bracket in enumerate(tax_brackets):
+        if i == len(tax_brackets) - 1:  # At the last tax bracket
+            tax_responsiblity += (income - rolling_income_total) * tax_bracket[1]
+        elif income < tax_bracket[0]:  # found out bracket
+            taxable_income = income - rolling_income_total
+            tax_responsiblity += taxable_income * tax_bracket[1]
+            break
+        else:
+            rolling_income_total += tax_bracket[0]
+            tax_responsiblity += tax_bracket[0] * tax_bracket[1]
+
+    return tax_responsiblity
 
 
-def get_input():
-    input("What's your annual income?\n>>> ")
+def calculate_taxes(income: float, tax_brackets: dict) -> dict:
+    tax_responsibilities = {}
+
+    for key in tax_brackets:
+        tax_responsibilities[key] = calculate_taxes_from_bracket(income, tax_brackets[key])
+
+    return tax_responsibilities
 
 
-# Make a main method for unit testing
-def main():
-    total_income = float(get_input())
-    tax = calculate_income_tax(total_income)
-    print(f"Total tax applicable at ${total_income} is ${tax}")
-
-
-if __name__ == '__main__':
-    main()
--- tax_calculator/test_tax_calculator.py
+++ tax_calculator/test_tax_calculator.py
@@ -1,24 +1,21 @@
-from tax_calculator import calculate, calculate_income_tax, main
+from .tax_calculator import calculate_taxes, tax_brackets
 from unittest import TestCase
-from unittest.mock import patch
 
 
 class TestCalculator(TestCase):
     def test_calculate(self):
-        result = calculate(100000, 10)
-        self.assertEqual(result, 10000)
+        taxes = calculate_taxes(0, tax_brackets)
+        self.assertEqual({'2001': 0, '2002': 0}, taxes)
 
-    def test_calculate_income_tax(self):
-        result = calculate_income_tax(500000)
-        self.assertEqual(result, 12500.0)
+        taxes = calculate_taxes(17000, tax_brackets)
+        self.assertEqual({'2001': 2400.0, '2002': 2900.0}, taxes)
 
-    @patch("tax_calculator.get_input", return_value=500000)
-    @patch("builtins.print")
-    def test_main(self, mock_patch_print, mock_patch_input):
-        main()
-        # Careful here: This gets called a million times trying to print the output of the unit test.
-        mock_patch_print.assert_called_with('Total tax applicable at $500000.0 is $12500.0')
+        taxes = calculate_taxes(100000, tax_brackets)
+        self.assertEqual({'2001': 37000.0, '2002': 39000.0}, taxes)
 
+    # assert calculate_taxes(0, tax_brackets) == 0
+    # assert calculate_taxes(17_000, tax_brackets) == 2_400
+    # assert calculate_taxes(100_000, tax_brackets) == 37_000
 
 
 
