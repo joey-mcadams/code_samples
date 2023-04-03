f5636298da13f05b97505cce88f336fb484c52f3
Joey McAdams <chakan2@hotmail.com>
2023-04-03 18:26:22 -0500

    added inheritance example

---
+++ inheritance/inheritance.py
@@ -0,0 +1,24 @@
+class A():
+    a = 1
+
+    def test(self):
+        print("A")
+
+class Mixin1():
+    def test(self):
+        print("Mix1")
+
+class Mixin2():
+    def test(self):
+        print("Mix1")
+
+class BaseTest(Mixin1, Mixin2):
+    pass
+
+A.a = 2
+
+print(A.a)
+BaseTest().test()
+
+
+
